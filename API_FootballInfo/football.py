925e5c419261f7cd8c3cc18168cc82b770781917
ZhorovIvan <jorov.and@yandex.ru>
2022-04-28 22:36:44 +0600

    added some new commands

--- API_FootballInfo/football.py
+++ API_FootballInfo/football.py
@@ -1,12 +1,14 @@
 import requests
 
-url = "https://api-football-beta.p.rapidapi.com/timezone"
+url = "https://sportscore1.p.rapidapi.com/teams/1/events"
+
+querystring = {"page":"1"}
 
 headers = {
-	"X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
+	"X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
 	"X-RapidAPI-Key": "18222134ecmsh6ecdd1171f1abedp130975jsn929db9efc17a"
 }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
