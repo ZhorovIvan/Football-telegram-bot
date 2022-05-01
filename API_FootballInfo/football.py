5b87439d7f2b3a66b997f9dd6c02bbf5f8ec840a
ZhorovIvan <jorov.and@yandex.ru>
2022-05-01 23:51:05 +0600

    form football api class

--- API_FootballInfo/football.py
+++ API_FootballInfo/football.py
@@ -1,14 +1,31 @@
 import requests
+import configparser
 
-url = "https://sportscore1.p.rapidapi.com/teams/1/events"
+class BettingApi():
 
-querystring = {"page":"1"}
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.read_config()
 
-headers = {
-	"X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
-	"X-RapidAPI-Key": "18222134ecmsh6ecdd1171f1abedp130975jsn929db9efc17a"
-}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    def read_config(self):
+        self.config.read("config.ini")
 
-print(response.text)
+
+    def get_club_events(self, club_name):
+        resp = self.get_response(self.config["FOOTBALL"]["events_url"])
+        for x in resp:
+            if x['team1'].lower() == 'barcelona': 
+                print(x['team1'] + ' vs ' +  x['team2'] + ' date ' + x['date_start'][:10])
+
+        
+    def get_response(self, url):
+        headers = {
+            "Authorization": self.config["FOOTBALL"]["auth"]
+        }
+        return requests.request("GET", url, headers=headers).json()
+
+
+test = BettingApi()
+
+test.get_club_events('f')
